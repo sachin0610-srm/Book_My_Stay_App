b76661d7b9cdac34b11003e6ccd4f332a9193123
sachin0610-srm <sachinlava14@gmail.com>
2026-03-16 13:50:34 +0530

    UC1:Application Entry & Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,6 @@
+import java.util.*;
 public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Booking Management System\nSystem initialized successfully");
+    }
 }
